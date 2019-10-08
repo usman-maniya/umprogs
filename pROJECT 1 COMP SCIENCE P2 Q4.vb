7f7acccddac75957d9a5674bb255ec5efcad77ce
usman-maniya <55947865+usman-maniya@users.noreply.github.com>
2019-10-08 21:24:40 +0500

    Add files via upload
    P2 comp science p22 . Conversion from pseudo code to binary

---
+++ pROJECT 1 COMP SCIENCE P2 Q4.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Newstring, Instring, nextchar As String
+        Dim index, selected, newvalue As Integer
+
+        Newstring = "0"
+        selected = 0
+        Console.Write("Enter new string: ")
+        Instring = Console.ReadLine
+        For index = 1 To Len(Instring)
+            nextchar = Mid(Instring, index, 1)
+            If nextchar < "0" Or nextchar > "9" Then
+                newvalue = Val(Newstring)
+
+                If newvalue > selected Then
+                    selected = newvalue
+                End If
+                Newstring = "0"
+            Else
+                Newstring = Newstring And nextchar
+
+            End If
+
+        Next
+        Console.WriteLine(selected)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
